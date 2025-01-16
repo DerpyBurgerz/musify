f8d37810a249ac4ff9b66bb0d02d70fb12779d36
jasper.alkema@icloud.com <jasper.alkema@icloud.com>
2025-01-16 19:07:20 +0100

    Fixed css

--- src/Frontend/App.js
+++ src/Frontend/App.js
@@ -1,13 +1,21 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/message')
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => setMessage(error.message));
+  }, []);
+
   return (
     <div className="App">
       <h1>musify.</h1>
-      <p>Your tool to discover new music.</p>
+      <p>{message}</p>
     </div>
   );
 }
 
 export default App;
-
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './Frontend/index.css';
 import App from './Frontend/App';
 import reportWebVitals from './Frontend/reportWebVitals';
+import './Frontend/App.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
