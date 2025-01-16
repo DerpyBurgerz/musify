dfe98ac9a22790341a5978e1c23b463b6b157c30
jasper.alkema@icloud.com <jasper.alkema@icloud.com>
2025-01-16 18:53:18 +0100

    Implemented start of backend

---
+++ src/Frontend/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>musify.</h1>
+      <p>Your tool to discover new music.</p>
+    </div>
+  );
+}
+
+export default App;
+
