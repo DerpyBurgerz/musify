c8ed2388dda81fb0414e9152df58b75ade28f4b3
DerpyBurgerz <144908293+DerpyBurgerz@users.noreply.github.com>
2025-02-12 22:10:40 +0100

    trying some more stuff, we should probably use more data for this, not sure if the cosine_similarity function works yet

--- src/Backend/recommender.py
+++ src/Backend/recommender.py
@@ -43,30 +43,32 @@ import numpy as np
 def cosine_similarity(a: [float], b: [float]) -> float:
     cos_similarity = np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
     return cos_similarity
-    
+
+
+pandas.set_option('display.max_columns', None)
+
 # https://georgepaskalev.medium.com/how-to-build-a-content-based-song-recommender-4346edbfa5cf
 def test_function():
-    column_names = ['track_name','tempo', 'energy']
+    column_names = ['track_name', 'energy', 'danceability', 'loudness', 'liveness', 'valence']
     df = pandas.read_csv('high_popularity_spotify_data.csv', usecols=column_names)
     
     similar_songs = df.copy()
     
     # only use tempo and energy for now
     # select only the tempo and energy columns
-    sound_properties = similar_songs.loc[:,['energy']]
+    sound_properties = similar_songs.loc[:,['energy', 'danceability', 'loudness', 'liveness', 'valence']]
 
     print(sound_properties)
-    similar_songs['similarity with song'] = cosine_similarity(sound_properties, [0.2])
+    similar_songs['similarity with song'] = cosine_similarity(sound_properties, [0.765, 0.588,-5.914 , 0.2, 0.492])
     similar_songs = similar_songs.sort_values(by=['similarity with song'], ascending=False)
     print(similar_songs.head())
     
     #line 555, GODS lol
     #0.765,145.988,0.588,gaming,-5.914,0.2,0.492,'League of Legends, NewJeans',4,0.0518,73,https://api.spotify.com/v1/tracks/210JJAa9nJOgNa0YNrsT5g,spotify:track:210JJAa9nJOgNa0YNrsT5g,GODS,Top Gaming Tracks,https://api.spotify.com/v1/audio-analysis/210JJAa9nJOgNa0YNrsT5g,210JJAa9nJOgNa0YNrsT5g,GODS,2023-10-04,3.23e-05,0rAaP1OBxVCn2cIUZNjGRs,0,3,220878,0.00685,210JJAa9nJOgNa0YNrsT5g,modern,audio_features,37i9dQZF1DWTyiBJ6yEqeu
- 
-print(cosine_similarity([1, 1], [-1, 1]))   
-#test_function()
-    
-    
+
+test_function()
+
+
 # energy,
 # tempo,
 # danceability,
