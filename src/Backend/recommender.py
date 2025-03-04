4d87089848760b158b28d99c5fbed4ad0c0b38b4
DerpyBurgerz <144908293+DerpyBurgerz@users.noreply.github.com>
2025-03-04 16:41:24 +0100

    added a function that creates an avarage of a list of songs

--- src/Backend/recommender.py
+++ src/Backend/recommender.py
@@ -45,9 +45,13 @@ def cosine_similarity(a: [float], b: [float]) -> float:
     cos_similarity = np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
     return cos_similarity
 
-def cosine_similarity_list(a: [float], b: [[float]]) -> float:
-    return sum([cosine_similarity(a, x) for x in b])/len(b)
-        
+def get_average_vector(ls: [[]]):
+    
+    out = [0] * len(ls[0])
+    for vector in ls:
+        out = [a + b for (a, b) in zip(out, vector)]
+    out = [x/len(ls) for x in out]
+    return out
 
 
 pandas.set_option('display.max_columns', None)
@@ -60,6 +64,9 @@ def test_function():
     # We normalize the bpm by dividing every bpm by the highest bpm found in the dataset
     max_bpm = df['tempo'].max()
     df['tempo']= df['tempo'].apply(lambda x: x/max_bpm) 
+    
+    our_songs = [[0.5, 0.5, 0.5, 0.5, 0.5, 130/200],[0.765, 0.588, 0.2, 0.492, 0.0518, 130/200],[0.765, 0.588, 0.2, 0.492, 0.0518, 130/200]]
+    our_songs = get_average_vector(our_songs)
     
     similar_songs = df.copy()
     test_song = df.copy()
@@ -74,7 +81,8 @@ def test_function():
     similar_songs = similar_songs.sort_values(by=['similarity with song'], ascending=False)
     print(tabulate(similar_songs.head()))
     
-    test_song['similarity with test song'] = cosine_similarity_list(sound_properties, [[0.5, 0.5, 0.5, 0.5, 0.5, 130/200],[0.765, 0.588, 0.2, 0.492, 0.0518, 130/200],[0.765, 0.588, 0.2, 0.492, 0.0518, 130/200]])
+    
+    test_song['similarity with test song'] = cosine_similarity(sound_properties, our_songs)
     test_song = test_song.sort_values(by=['similarity with test song'], ascending=False)
     print(tabulate(test_song.head()))
     
