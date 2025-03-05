696116c044782418a4bd27db82d87313728592c3
DerpyBurgerz <samuel@timetoknow.nl>
2025-03-05 14:30:36 +0100

    smol change

--- src/Backend/recommender.py
+++ src/Backend/recommender.py
@@ -53,8 +53,10 @@ def get_average_vector(ls: [[]]):
     out = [x/len(ls) for x in out]
     return out
 
+def get_recommendations(df: pandas.DataFrame, songs: [[float]]):
 
-pandas.set_option('display.max_columns', None)
+
+
 
 # https://georgepaskalev.medium.com/how-to-build-a-content-based-song-recommender-4346edbfa5cf
 def test_function():
@@ -91,6 +93,7 @@ def test_function():
     #energy,tempo,danceability,playlist_genre,loudness,liveness,valence,track_artist,time_signature,speechiness,track_popularity,track_href,uri,track_album_name,playlist_name,analysis_url,track_id,track_name,track_album_release_date,instrumentalness,track_album_id,mode,key,duration_ms,acousticness,id,playlist_subgenre,type,playlist_id
     #0.765 ,145.988,0.588      ,gaming       ,-5.914,0.2,0.492,'League of Legends, NewJeans',4,0.0518          ,73,https://api.spotify.com/v1/tracks/210JJAa9nJOgNa0YNrsT5g,spotify:track:210JJAa9nJOgNa0YNrsT5g,GODS,Top Gaming Tracks,https://api.spotify.com/v1/audio-analysis/210JJAa9nJOgNa0YNrsT5g,210JJAa9nJOgNa0YNrsT5g,GODS,2023-10-04,3.23e-05,0rAaP1OBxVCn2cIUZNjGRs,0,3,220878,0.00685,210JJAa9nJOgNa0YNrsT5g,modern,audio_features,37i9dQZF1DWTyiBJ6yEqeu
 
+pandas.set_option('display.max_columns', None)
 test_function()
 
 
